2f9ae9621bc8ed93f7e41a7626453f9463c3086a
Felix Kulow <felix.kulow@th-wildau.de>
2024-05-14 13:46:03 +0200

    Projekt Start

---
+++ hillClimber.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 14:59:48 2023
+
+@author: ThomasKopsch
+"""
+
+import pandas as pd
+
+
+# The task is to implement the hillClimber algorithm.
+# Find a 'good' solution to the Travelling salesman
+# problem using the hillClimber algorithm.
+#
+# The distances of 80 large German cities is provided.
+# Each city is encoded with a number 1-80.
+# You can find a legend in the accompanieng documents.
+
+
+#########################################################
+#########################################################
+######### NO NOT CHANGE THE FOLLOWING CODE! #############
+#########################################################
+#########################################################
+
+def readDistances():
+    """
+    This method reads the example data and splits
+    the data randomly into two sets to create a
+    training and test data set.
+    """
+    
+    # Read the example Data from the file.
+    return pd.read_csv('distances.csv', sep=',', header=0)
+    
+distanceTable = readDistances()
+
+def getDistance(cityA, cityB):
+    return distanceTable.loc[cityA-1][cityB]
+
+
+#########################################################
+#########################################################
+########### NO NOT CHANGE THE FORMER CODE! ##############
+#########################################################
+#########################################################
+
+# Example: The distance between Berlin (1) and Hamburg (2)
+print(f"The distance between Berlin and Hamburg is {getDistance(1,2)}km.")
+
+
+

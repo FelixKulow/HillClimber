125fd15f29f10bb7507b4f5f314f047d6468371f
Felix Kulow <felix.kulow@th-wildau.de>
2024-05-14 15:49:06 +0200

    Update

--- hillClimber.py
+++ hillClimber.py
@@ -51,20 +51,16 @@ def getDistance(cityA, cityB):
 print(f"The distance between Berlin and Hamburg is {getDistance(1,2)}km.")
 
 
-# Zeile 54 bis 64: Gesamtdistanz ausrechnen (ohne Sortierung). 
-
-
-
-
-
 def getTotalDistance(path): # Definition von "getTotalDistance"
     TotalDistance = 0 # Die gesamte Distanz wird gleich 0 gesetzt
     for i in range(len(path)-1): # Mit range wird der Bereich definiert, aus welchen Stätdten die Distanz errechnet werden soll, da wir die gesamte Distanz errechnen wollen Benötigen wir auch die gesamte liste von insgesamt 80 Einträgen.
         TotalDistance += getDistance(path[i], path[i+1]) # Durch die Vaariable i können wir alle Positionen nacheinander aufaddieren.
-    
     return TotalDistance 
 
 initialPath = list(range(1,81))
-print(getTotalDistance( initialPath)) #
+print(f"Die gesamte Distanz aus der unsorterten Liste beträgt {getTotalDistance( initialPath)} Km") # Ausgabe der unsortierten gesamten Distanz
+
+
+
 
 
